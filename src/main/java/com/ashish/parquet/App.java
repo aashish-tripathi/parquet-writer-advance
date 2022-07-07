97fd796c046666b75d8f0a56aadf91e85e9e7336
aashish-tripathi <aashish.tripathi@gmail.com>
2022-07-08 00:08:58 +0530

    updates

--- src/main/java/com/ashish/parquet/App.java
+++ src/main/java/com/ashish/parquet/App.java
@@ -3,6 +3,7 @@ package com.ashish.parquet;
 import com.ashish.parquet.avro.Trade;
 import org.apache.avro.Schema;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.avro.AvroParquetWriter;
@@ -11,54 +12,48 @@ import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.parquet.hadoop.util.HadoopOutputFile;
 import org.apache.parquet.io.InputFile;
+import org.apache.parquet.io.OutputFile;
 
 import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        Schema avroSchema = Trade.getClassSchema();
-        Trade[] trades = new Trade[]{Trade.newBuilder()
-                .setTime(System.currentTimeMillis())
-                .setExchange("NSE")
-                .setPrice(ThreadLocalRandom.current().nextDouble(99.0))
-                .setSize(ThreadLocalRandom.current().nextLong(999))
-                .setSymbol("ICICI").build(), Trade.newBuilder()
-                .setTime(System.currentTimeMillis())
-                .setExchange("NSE")
-                .setPrice(ThreadLocalRandom.current().nextDouble(99.0))
-                .setSize(ThreadLocalRandom.current().nextLong(999))
-                .setSymbol("ICICI").build(), Trade.newBuilder()
-                .setTime(System.currentTimeMillis())
-                .setExchange("NSE")
-                .setPrice(ThreadLocalRandom.current().nextDouble(99.0))
-                .setSize(ThreadLocalRandom.current().nextLong(999))
-                .setSymbol("ICICI").build(), Trade.newBuilder()
-                .setTime(System.currentTimeMillis())
-                .setExchange("NSE")
-                .setPrice(ThreadLocalRandom.current().nextDouble(99.0))
-                .setSize(ThreadLocalRandom.current().nextLong(999))
-                .setSymbol("ICICI").build()};
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         Path filePath = new Path("md/Trade.parquet");
-        writeParquet(avroSchema, trades, filePath);
+        writeParquet(filePath);
+        Thread.sleep(5000l);
         readParquet(filePath);
     }
 
-    private static void writeParquet(Schema avroSchema, Trade[] dataToWrite, Path filePath) {
-        try (ParquetWriter<Trade> writer = AvroParquetWriter.<Trade>builder(filePath)
+    private static void writeParquet(Path filePath) throws IOException {
+        Schema avroSchema = Trade.getClassSchema();
+        OutputFile outputFile = HadoopOutputFile.fromPath(filePath, new Configuration());
+        try (ParquetWriter<Trade> writer = AvroParquetWriter.<Trade>builder(outputFile)
                 .withSchema(avroSchema).withCompressionCodec(CompressionCodecName.SNAPPY)
                 .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
                 .withConf(new Configuration())
                 .build()) {
-            for (Trade obj : dataToWrite) {
-                writer.write(obj);
+            long start = System.nanoTime();
+            for (int i = 0; i <2000000 ; i++) {
+                writer.write(getTrade());
             }
+            long end = System.nanoTime();
+            System.out.println("Total time take to write 2 million records (ns)"+(end-start));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Trade getTrade() {
+        return Trade.newBuilder()
+                .setTime(System.currentTimeMillis())
+                .setExchange("NSE")
+                .setPrice(ThreadLocalRandom.current().nextDouble(99.0))
+                .setSize(ThreadLocalRandom.current().nextLong(999))
+                .setSymbol("ICICI").build();
     }
 
     private static void readParquet(Path filePath) throws IOException {
@@ -67,7 +62,6 @@ public class App {
             Trade trade;
             while ((trade = parquetReader.read()) != null) {
                 System.out.println(trade);
-                Thread.sleep(1000);
             }
         } catch (Exception e) {
             e.printStackTrace();
